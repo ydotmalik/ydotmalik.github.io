210081d2861882ced6dc1913d5eecd3a836715d7
Yasir Malik <y.malik@ymail.com>
2017-11-05 23:50:01 -0500

    First commit

---
+++ reo/form_handler.php
@@ -0,0 +1,2 @@
+<?php header('Access-Control-Allow-Origin: *'); ?>
+<p>Server got <?php echo $_POST['field1']; ?> and <?php echo $_POST['field2']; ?></p>
---
+++ reo/long_load.php
@@ -0,0 +1,2 @@
+<?php header('Access-Control-Allow-Origin: *'); sleep(1); ?>
+<p>This took a long time to load, but the loader made you feel better about the wait.</p>
